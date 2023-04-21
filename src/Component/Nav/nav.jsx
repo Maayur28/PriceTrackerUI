b77e9c0091a0f27f7c532b9cdbfb6af3063a0928
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-04-21 20:55:03 +0530

    added notify fix

--- src/Component/Nav/nav.jsx
+++ src/Component/Nav/nav.jsx
@@ -1,12 +1,23 @@
 import React, { useEffect, useState } from "react";
-import { Layout, Menu, Image, Dropdown, Empty } from "antd";
+import {
+  Layout,
+  Menu,
+  Image,
+  Dropdown,
+  Empty,
+  Avatar,
+  Divider,
+  Button,
+  Typography,
+} from "antd";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
 import { BellOutlined } from "@ant-design/icons";
 import { handleLoginLogout } from "../../Util";
 import "./nav.css";
 import { addNotification, getNotification } from "../../Cache";
-import { contructItems } from "./navUtil";
+
+const { Title, Text } = Typography;
 
 const { Header } = Layout;
 
@@ -29,7 +40,7 @@ const Nav = () => {
       notifyData !== undefined &&
       notifyData.length > 0
     ) {
-      contructItems({ notifyData: notifyData, setItems: setItems });
+      contructItems();
     } else {
       let item = [];
       let obj = {};
@@ -39,6 +50,7 @@ const Nav = () => {
       localStorage.removeItem("priceTracker_notification");
       setItems(item);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [notifyData]);
 
   useEffect(() => {
@@ -87,6 +99,128 @@ const Nav = () => {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const dismissNotification = async () => {
+    if (
+      Cookies.get("accessToken") !== undefined &&
+      Cookies.get("refreshToken") !== undefined
+    ) {
+      fetch("https://price-tracker-auth.vercel.app/dismissNotification", {
+        method: "POST",
+        body: JSON.stringify({
+          accessToken: Cookies.get("accessToken"),
+          refreshToken: Cookies.get("refreshToken"),
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      })
+        .then(async (response) => {
+          if (response.status >= 200 && response.status <= 299) {
+            return response.json();
+          } else {
+            const text = await response.text();
+            throw new Error(text);
+          }
+        })
+        .then((data) => {
+          if (data.success === true) {
+            setNotifyData([]);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
+  const contructItems = () => {
+    let item = [];
+    if (
+      notifyData != null &&
+      notifyData !== undefined &&
+      notifyData.length > 0
+    ) {
+      let obj = {};
+      obj.key = "header_notify";
+      obj.label = (
+        <>
+          <div style={{ cursor: "default" }}>
+            <Button
+              onClick={dismissNotification}
+              type="text"
+              style={{ position: "absolute", right: "0", top: "0" }}
+            >
+              Dismiss
+            </Button>
+          </div>
+          <Divider
+            style={{ margin: "0", marginTop: "30px", marginBottom: "2px" }}
+          />
+        </>
+      );
+      item.push(obj);
+      notifyData.forEach((element) => {
+        let difference_ms = Date.now() - element.date;
+        //take out milliseconds
+        difference_ms = difference_ms / 1000;
+        let seconds = Math.floor(difference_ms % 60);
+        difference_ms = difference_ms / 60;
+        let minutes = Math.floor(difference_ms % 60);
+        difference_ms = difference_ms / 60;
+        let hours = Math.floor(difference_ms % 24);
+        let days = Math.floor(difference_ms / 24);
+        let obj = {};
+        obj.key = element._id;
+        obj.label = (
+          <div
+            style={{ display: "flex" }}
+            onClick={() => openInNewTab(element.url)}
+          >
+            <Avatar
+              shape="square"
+              size={56}
+              src={<img width="60px" src={element.image} alt="avatar" />}
+            />
+            <div style={{ marginLeft: "10px" }}>
+              <Title style={{ margin: "0" }} level={5}>
+                Price dropped by{" "}
+                <span style={{ color: "#7f4574", fontWeight: "bolder" }}>
+                  ₹{element.price}
+                </span>
+              </Title>
+              <Text ellipsis={true} style={{ maxWidth: "250px" }}>
+                {element.title}
+              </Text>
+              <div>
+                <Text type="secondary">
+                  {days === 0
+                    ? hours === 0
+                      ? minutes === 0
+                        ? `${seconds} seconds ago`
+                        : `${minutes} minutes ago`
+                      : `${hours} hours ago`
+                    : `${days} days ago`}
+                </Text>
+              </div>
+            </div>
+          </div>
+        );
+        item.push(obj);
+      });
+    } else {
+      let obj = {};
+      obj.key = "no_notification";
+      obj.label = <Empty />;
+      item.push(obj);
+      localStorage.removeItem("priceTracker_notification");
+    }
+    setItems(item);
+  };
+
+  const openInNewTab = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
     <Layout style={{ height: "60px", position: "relative" }}>
