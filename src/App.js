19e7b02d796849b890cb1885da52fa26a52dc4a0
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-08-18 18:12:11 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
       <Title level={2}>
         We are migrated to new website with better UI and lots of cool features
       </Title>
+      <Title level={2}>Have a look!!! Its still in progress...</Title>
       <Link
         href="https://trackmyprice.in/"
         id="latestdeals"
